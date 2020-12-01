cc07b2fabb70c5d78d767f18dd51444c4251929d
Paul Jaenichen <PNJaenichen@gmail.com>
2020-12-01 10:13:59 -0500

    Update day1.py
    Updated for proper output

--- day1.py
+++ day1.py
@@ -3,27 +3,32 @@ with open('Day1a.txt') as reports:
     for i in reports.readlines():
         expenses.append(int(i.strip()))
 
+def threeRes(ex, it):
+    remain = 2020 - it
+    j = 0
+    while j < leng:
+        if ex[j] == item:
+            j += 1
+        elif remain - ex[j] in ex:
+            return item * expenses[j] * (remain - expenses[j])
+        else:
+            j += 1
+    return ""
+
+
 leng = len(expenses)
-j = 0
 
 for item in expenses:
     remain = 2020 - item
     if remain in expenses:
         twoEntryResult = item * remain
-        print("The two entry result is " + twoEntryResult)
+        print(f"The two entry result is {twoEntryResult}")
         break
     else:
         continue
 
 for item in expenses:
-    remain = 2020 - item
-    j = 0
-    while j < leng:
-        if expenses[j] == item:
-            j += 1
-        elif remain - expenses[j] in expenses:
-            threeEntryResult = item * expenses[j] * (remain - expenses[j]))
-            print("The three entry result is " + threeEntryResult)
-            break
-        else:
-            j += 1
+    threeEntryResult = threeRes(expenses, item)
+    if threeEntryResult:
+        print(f"The three entry result is {threeEntryResult}")
+        break
