1556bbd871baad9a28db7f83db95f0f717fe0d74
Paul Jaenichen <PNJaenichen@gmail.com>
2020-12-04 11:14:49 -0500

    Added Comments and Renamed File
    Added comments to explain some of the portions of the code.

--- Day1.py
+++ Day1.py
@@ -1,8 +1,10 @@
+# Open the input file and read the information to a list of expenses
 with open('Day1a.txt') as reports:
     expenses = []
     for i in reports.readlines():
         expenses.append(int(i.strip()))
 
+# Function to find three numbers within a list that equal a certain value
 def threeRes(ex, it):
     remain = 2020 - it
     j = 0
@@ -15,11 +17,13 @@ def threeRes(ex, it):
             j += 1
     return ""
 
-
+# Get the length of the expenses list
 leng = len(expenses)
 
+# Cycle through the list of expenses to find two that equal a certain value
 for item in expenses:
     remain = 2020 - item
+    # Once you find the remainder, search to see if that remainder is within the list and then break if it's found
     if remain in expenses:
         twoEntryResult = item * remain
         print(f"The two entry result is {twoEntryResult}")
@@ -27,6 +31,7 @@ for item in expenses:
     else:
         continue
 
+# Cycle through the list and run them through the three number function
 for item in expenses:
     threeEntryResult = threeRes(expenses, item)
     if threeEntryResult:
