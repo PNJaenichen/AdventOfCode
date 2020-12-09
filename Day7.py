a24b71e9535a60716dea019599d45a0ae8399591
PNJaenichen <PNJaenichen@gmail.com>
2020-12-08 22:22:03 -0500

    Working Code with lots of help.

--- Day7.py
+++ Day7.py
@@ -1,14 +1,13 @@
 def key_check(key, rule):
-    for keys in rule[key]:
-        if 'shiny gold' in list(rule[key].keys()):
-            return True
-        elif key_check(keys,rule):
-            return True
-        else:
-            return key_check(keys,rule)
+    return any('shiny gold' in list(rule[key].keys()) or 
+        key_check(keys, rule) for keys in rule[key])
+
+def count_bag(key, rule):
+    return sum([n + n * count_bag(keys, rule) 
+        for keys, n in rule[key].items()])
 
 # Get input
-with open('Day7a.txt') as f:
+with open('Day7b.txt') as f:
     instructions = []
     for line in f.readlines():
         instructions.append(line.strip())
@@ -29,13 +28,15 @@ for i in instructions:
             else:
                 parts[1][j] = parts[1][j].replace(' bag', '')
             parts[1][j] = parts[1][j].split(' ', 1)
-            bag[parts[1][j][1]] = parts[1][j][0]
+            bag[parts[1][j][1]] = int(parts[1][j][0])
     rules[parts[0]] = bag
 
 gold_count = []
 
 for key in rules:
-    if key_check(key, rules):
+    if key_check(key,rules):
         gold_count.append(key)
-        
-print(gold_count, len(gold_count))
+
+print(f'There are {len(gold_count)} bags that can hold a shiny gold bag')
+
+print(count_bag('shiny gold', rules))
