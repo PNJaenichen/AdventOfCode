0d8b37298844429b146c633f1be684946f2aa8b1
PNJaenichen <PNJaenichen@gmail.com>
2020-12-18 23:09:10 -0500

    Day 13 with lots of help for Part 2

--- 2020_AOC/Day13.py
+++ 2020_AOC/Day13.py
@@ -31,4 +31,34 @@ for x in buses:
             continue
 
 # Print the bus ID by it's wait time for Part 1 Solution
-print(lowest[0] * lowest[1])
+print(f"You'll be waiting for Bus #{lowest[0]} for {lowest[1]} minutes ({lowest[0] * lowest[1]}).")
+
+# Take the bus ideas and gather those needed along with their difference from 0-time
+input_data = buses
+bus_ids = []
+for i, id in enumerate(input_data):
+    if id != 'x':
+        bus_ids.append((id, (id-i) % id))
+
+# The initial timestamp can't be less than the first value
+tmp = bus_ids[0][0]
+timestamp, i = tmp, 0
+
+# Chinese Remainder Theorem ... which honestly is magic. There are symbols on the wiki
+# that I have never seen
+loop = True
+while loop:
+    id, remainder = bus_ids[i+1]
+    if (timestamp) % id  == remainder:
+    # if the remainder from the enumeration is the same as with the timestamp ...
+    # and we are at the end of the list of buses ...
+        if i == len(bus_ids) - 2:
+            # We've found the timestamp, print it and break the loop
+            print(f"Timestamp of the earliest bus with subsequent offset: {timestamp}")
+            loop = False
+        # If not, then we increase to the next bus ID
+        tmp *= id
+        i += 1
+    # If the timestamp and i,id remainders don't match, increase the timestamp and 
+    # try again
+    timestamp += tmp
